dca46d1e17aa88a69611ef7aefc1522387a08246
Lauren Richert <lsrichert@gmail.com>
2018-05-23 21:25:17 -0500

    added chickenMonkey

---
+++ main.js
@@ -0,0 +1,10 @@
+for (let i = 0; i <= 100; i++) { 
+    if (i % 5 ===0) {
+        console.log("Chicken", i)
+    }
+      else if (i % 7 ===0) {
+        console.log("Monkey", i)
+    
+    
+    } else {
+        console.log(i)
